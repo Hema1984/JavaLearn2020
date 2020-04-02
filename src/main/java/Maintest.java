4d9b5520edf216aac715fedfa41b33280a488de3
Hema1984 <hemapokhariya@gmail.com>
2020-04-02 12:29:26 -0400

    Made changes as Jenkins

--- src/main/java/Maintest.java
+++ src/main/java/Maintest.java
@@ -1,5 +1,6 @@
 public class Maintest {
     public static void main(String[] args) {
         System.out.println("Hello learning git");
+        System.out.println("Hello learning Jenkins");
     }
 }
